99ed50aae3ed070a54d88da8e29418c764cf3d10
Apurva Sijaria <apurvasijaria.iitkgp@gmail.com>
2016-07-04 01:39:16 +0530

    Update ExposureDis.m

--- ExposureDis.m
+++ ExposureDis.m
@@ -3,7 +3,7 @@ function [approx]=ExposureDis (dom_node, int_node, countries, bankclaims )
 % approximating the individual bank-to-bank claims by dividing
 % the aggregated claims of all domestic banks by the number of UK banks (17) and
 % the number of international banks per country (12).
-approx= bankclaims/ (dom_node+ (int_node/countries));
+approx= bankclaims./ (dom_node+ (int_node/countries));
 
 end
 
