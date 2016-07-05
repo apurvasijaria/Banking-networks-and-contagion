8045163ff7a013284314f43e9f74ec03c9a45958
App-S <apurvasijaria.iitkgp@gmail.com>
2016-07-05 13:31:05 +0530

    update

---
+++ shock.m
@@ -0,0 +1,56 @@
+
+function [ repaymentAll, Nsurvive] = shockNetwork( Lij, S, R, r, f, fLambda, leverage, endCondition)
+
+%------------------------------------------------------------------------%
+%   lending, borrowing, assets, liquid assets, senior liability, and
+%   liability are defined according to criticaldegree2.pdf, given fixed
+%   input parameters f, flambda, and leverage
+%------------------------------------------------------------------------%
+lending = sum(Lij,2);
+borrowing = sum(Lij)';
+assets = lending/f;
+liquidAssets = fLambda*assets;
+seniorliability = (leverage-1)/(leverage*f)*lending - borrowing;
+liabilities = borrowing + seniorliability;
+
+% the return on investment and junior debt are both calculated for a given
+% shock S
+actualROI = (R*S-1).*max(liabilities,borrowing);
+juniorDebt = r*borrowing;
+
+%------------------------------------------------------------------------%
+%   Calculation of the fixed point for repayments made
+%   X1 and X2 are the current and previous repayment fractions, used to
+%   determine when the end condition has been reached.
+%------------------------------------------------------------------------%
+%%% Assume that the repayment fraction is 1 initially
+repayFrac = ones(size(lending));
+X1 = repayFrac;
+repayment = actualROI + liquidAssets - seniorliability + r*Lij*repayFrac;
+repayFrac = max(min(repayment,juniorDebt),0)./(borrowing*r);
+%%% if no borrowing is done, dividing by 0 above gives NAN, so it is here
+%%% set to 1 to fix that problem
+repayFrac(borrowing==0) = 1;
+X2 = repayFrac;
+
+
+%%% Repeat above calculations updating the repayment fraction until the end
+%%% condition has been reached
+while( max(abs(X1-X2)./X2) > endCondition )
+    repayment = actualROI + liquidAssets - seniorliability + r*Lij*repayFrac;
+    repayFrac = max(min(repayment,juniorDebt),0)./(borrowing*r);
+    repayFrac(borrowing==0) = 1;
+    X1 = X2;
+    X2 = repayFrac;
+end
+
+%------------------------------------------------------------------------%
+% Calculate the number of surviving banks, as defined by those having a
+% positive net worth after all collapsing banks have collapsed, or as not
+% having lent or borrowed anything, (ie, not part of the network)
+%------------------------------------------------------------------------%
+netWorth = actualROI + liquidAssets - seniorliability + Lij*r*repayFrac - borrowing*r.*repayFrac;
+Nsurvive = sum(netWorth>0|(lending==0&borrowing==0));
+repaymentAll = repayFrac.*borrowing;
+
+end
