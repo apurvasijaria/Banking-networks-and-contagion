6ed5e808e42d9681d8def42664b8c0cc7eada0d7
App-S <apurvasijaria.iitkgp@gmail.com>
2016-07-11 15:41:35 +0530

    update

--- shockfails.m
+++ shockfails.m
@@ -1,7 +1,11 @@
 
 %individual bank data is unavailable
 
-
+ dom_node=6;     %test data
+ int_node=2;     %test data
+ firm_node=3;     %test data
+ no_network=1;   %test data
+ 
 % dom_loan=loan holding on domestic banks on average (in mils);
 % dom_eq=equity holding on domestic banks on average (in mils)
 % int_loan=loan holding on international banks on average (in mils)
@@ -25,9 +29,9 @@ int_dom_eq=4000;
 
 %corporate default probabilities treat the default probability in
 %investment (A) grade category
-% as having a mean and standard deviation of hPDIGi = 8.65 Ã— 10?5 and ?IG =
+% as having a mean and standard deviation of hPDIGi = 8.65 × 10?5 and ?IG =
 % 2* 10?5, respectively. The non-investment grade (BB) category has a mean
-% and standard deviation of hPDNIGi = 6.3 Ã— 10?3 and ?NIG = 6.1 Ã— 10?4,
+% and standard deviation of hPDNIGi = 6.3 × 10?3 and ?NIG = 6.1 × 10?4,
 % respectively. The proportion of firms that are investment grade
 % (speculative grade) within the system is 0.7 (0.3). We take the firm LGD
 % to be 35%.
@@ -43,7 +47,7 @@ lgd=0.35;
 % setting the parameter = 0.5 lambda=Liquidity discount parameter,we adopt
 % a value for lambda consistent with a more significant price impact.
 % psi=Macroeconomic feedback parameter, phi=we (somewhat arbitrarily) set 
-% = 6.25 Ã— 10?5 as a working hypothesis.
+% = 6.25 × 10?5 as a working hypothesis.
 
 
 leverage=0.04;
@@ -65,6 +69,48 @@ networkType = 'random';
 alphaAll = fliplr([.01:.01:.99]);  % for random
 Nshocks = 1;
 
-%% the code is yet to be updated.
+%------------------------------------------------------------------------%
+% Creating matrices to fill in loops
+%------------------------------------------------------------------------%
+survived = zeros(length(alphaAll),Navg);
+meanDegree = zeros(length(alphaAll),Navg);
+%------------------------------------------------------------------------%
+% Loop over things we are interested in
+%------------------------------------------------------------------------%
+%%% Look at each network parameter, alpha
+tic
+degreeDist = zeros(length(alphaAll),n+1);
+P = zeros(size(alphaAll));
+for k = 1:length(alphaAll)
+    alpha = alphaAll(k);
+    p = 1-alpha;
+    degreeDist(k,:) = (p*n).^[0:n]./factorial([0:n]).*exp(-p*n);
+    x = round(kstar);
+    P(k) = sum(degreeDist(k,1:x));
+    %%% Look at several networks and average over them, to smooth out
+    %%% randomness of network creation and shocks
+    for m = 1:Navg
+        network = newnetwork(dom_node,int_node,firm_node,1);
+        degree = sum(ceil(network),2);
+        
+        %%% pick random node to shock
+        s = randi(Nshocks,1);
+        S = ones(n,1);
+        S(s) = shockSize;
+        
+        %%% find fixed point and resulting parameters of networks
+        [xij,Nsurvive] = fails(network,endCondition,dom_node,int_node,firm_node);
+        survived(k,m) = Nsurvive;
+        meanDegree(k,m) = mean(degree);
+    end
+end
+toc
+%------------------------------------------------------------------------%
+% Plotting things
+%------------------------------------------------------------------------%
 
+meanDegreeDist = trapz([0:n],repmat([0:n],99,1)'.*degreeDist')./trapz([0:n],degreeDist');
+plot(meanDegreeDist,1+meanDegreeDist.*P,'*',mean(meanDegree,2),n-mean(survived,2),'*')
+xlabel('Mean Degree')
+ylabel('Number Failed')
 
